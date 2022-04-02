e2e24097ae8978018a19f0b3525b522e5f595d78
sbx_user1051 <sbx_user1051@169.254.136.165>
2022-04-02 11:20:45 +0000

    Sat Apr 02 11:20:45 UTC 2022

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -1,9 +1,11 @@
+
+import sys
+print(sys.path)
 from lib.solutions.SUM import sum_solution
-
-
 class TestSum():
     def test_sum(self):
         assert sum_solution.compute(1, 2) == 3
         assert sum_solution.compute(3, 2) == 5
         assert sum_solution.compute(3, 1) == 5 #wrong test; let's see what happens
+
 
