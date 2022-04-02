d072e9e948ccbd8e54147d03829c4d995c8837a6
sbx_user1051 <sbx_user1051@169.254.136.165>
2022-04-02 11:27:16 +0000

    Sat Apr 02 11:27:16 UTC 2022

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -3,7 +3,8 @@ class TestSum():
     def test_sum(self):
         assert sum_solution.compute(1, 2) == 3
         assert sum_solution.compute(3, 2) == 5
-        assert sum_solution.compute(3, 1) == 5 #wrong test; let's see what happens
+        assert sum_solution.compute(3, 1) == 4
+
 
 
 
