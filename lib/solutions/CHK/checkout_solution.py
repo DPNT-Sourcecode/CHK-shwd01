b2cf0d026e8f6faa325d892a6fac2a8a036d2e07
sbx_user1051 <sbx_user1051@169.254.147.25>
2022-04-02 13:31:01 +0000

    Sat Apr 02 13:31:01 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -6,8 +6,10 @@ from re import M
 # skus = unicode string
 
 min_price=INF
+prices={"A":50, "B":30, "C":20, "D":15, "E":40}
+offers={"AAA": 130,"AAAAA":200, "BB": 45, "EEB":80}
 
-def backtracking(idx, offers, prices, part_price, missing):
+def backtracking(idx, part_price, missing):
     if missing.keys()==[]:
         if min_price>part_price: min_price=part_price
         return
@@ -18,7 +20,15 @@ def backtracking(idx, offers, prices, part_price, missing):
         if min_price>part_price: min_price=part_price
         return
 
-    offered=offers
+    offered=offers.keys()[idx]
+    tgt_price=offers[offered]
+    backtracking(idx+1, part_price, missing)
+    #we only apply the offer if it makes sense
+    if set(Counter(offered)).intersection(set(missing.keys())):
+        part_price+=tgt_price
+        for key, element in Counter(offered):
+            #if key in m 
+
 
 
     if not set(buy.keys()) <= set(prices.keys()): return -1
@@ -40,12 +50,12 @@ def backtracking(idx, offers, prices, part_price, missing):
 
 def checkout(skus):
     buy=Counter(skus)
-    prices={"A":50, "B":30, "C":20, "D":15, "E":40}
-    offers={"AAA": 130,"AAAAA":200, "BB": 45, "EEB":80}
-    backtracking(0, offers, prices, 0, buy)
+
+    backtracking(0, 0, buy)
     return min_price
 
 
 
 assert checkout("AAA")==130
+
 
