7a4c21bee382acc830122807e7958dd3a4752a3a
sbx_user1051 <sbx_user1051@169.254.147.25>
2022-04-02 13:58:01 +0000

    Sat Apr 02 13:58:01 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -14,20 +14,25 @@ def backtracking(idx, part_price, missing_aux):
     missing=missing_aux.copy()
     if missing.keys()==[]:
         if min_price>part_price: min_price=part_price
+        print(min_price)
         return
 
     if idx==len(offers):
         for key, element in missing.items():
             if element>0: part_price+=element*prices[key]    
         if min_price>part_price: min_price=part_price
+        print(min_price)
         return
 
     offered=list(offers.keys())[idx]
     tgt_price=offers[offered]
+
     backtracking(idx+1, part_price, missing.copy())
     
 
     if set(Counter(offered)).intersection(set(list(missing.keys()))):
+        print(offered)
+        print(missing)
         part_price+=tgt_price
         for key, element in Counter(offered).items():
             if key in missing:
@@ -50,3 +55,10 @@ assert checkout("AAA")==130
 assert checkout("AAAAAA")==250
 assert checkout("AAAAA")==200
 assert checkout("AA")==100
+
+
+assert checkout("BB")==45
+assert checkout("B")==30
+assert checkout("bBB")==-1
+print('debug')
+assert checkout("BBBBB")==120
