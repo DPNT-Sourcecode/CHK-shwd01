7390d28d978cb06dbc9227410b2226e66d901c82
sbx_user1051 <sbx_user1051@169.254.147.25>
2022-04-02 13:25:01 +0000

    Sat Apr 02 13:25:01 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,5 +1,6 @@
 from asyncio.windows_events import INFINITE
 from collections import Counter
+from re import M
 
 # noinspection PyUnusedLocal
 # skus = unicode string
@@ -7,21 +8,17 @@ from collections import Counter
 min_price=INF
 
 def backtracking(idx, offers, prices, part_price, missing):
+    if missing.keys()==[]:
+        if min_price>part_price: min_price=part_price
+        return
 
+    if idx==len(offers):
+        for key, element in missing.items():
+            if element>0: part_price+=element*prices[key]    
+        if min_price>part_price: min_price=part_price
+        return
 
-
-def checkout(skus):
-    buy=Counter(skus)
-    prices={"A":50, "B":30, "C":20, "D":15, "E":40}
-    offers={"AAA": 130,"AAAAA":200, "BB": 45, "EEB":80}
-    backtracking(0, offers, prices, part_price, missing)
-    return min_price
-
-
-
-
-
-
+    offered=offers
 
 
     if not set(buy.keys()) <= set(prices.keys()): return -1
@@ -37,6 +34,18 @@ def checkout(skus):
         price+=price_item
     print(price)
     return price
+
+
+
+
+def checkout(skus):
+    buy=Counter(skus)
+    prices={"A":50, "B":30, "C":20, "D":15, "E":40}
+    offers={"AAA": 130,"AAAAA":200, "BB": 45, "EEB":80}
+    backtracking(0, offers, prices, 0, buy)
+    return min_price
+
+
 
 assert checkout("AAA")==130
 
