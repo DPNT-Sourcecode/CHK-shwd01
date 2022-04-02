a01670361d62e4362fc1632bbd19874e5ade408d
sbx_user1051 <sbx_user1051@169.254.147.25>
2022-04-02 13:55:01 +0000

    Sat Apr 02 13:55:01 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,14 +3,15 @@ from collections import Counter
 # noinspection PyUnusedLocal
 # skus = unicode string
 
-min_price= float("inf")
+min_price=float("inf")
 prices={"A":50, "B":30, "C":20, "D":15, "E":40}
 offers={"AAA": 130,"AAAAA":200, "BB": 45, "EEB":80}
 
-def backtracking(idx, part_price, missing):
+def backtracking(idx, part_price, missing_aux):
     global prices
     global offers
     global min_price
+    missing=missing_aux.copy()
     if missing.keys()==[]:
         if min_price>part_price: min_price=part_price
         return
@@ -23,9 +24,8 @@ def backtracking(idx, part_price, missing):
 
     offered=list(offers.keys())[idx]
     tgt_price=offers[offered]
-    backtracking(idx+1, part_price, missing)
-    print(offered)
-    print(missing)
+    backtracking(idx+1, part_price, missing.copy())
+    
 
     if set(Counter(offered)).intersection(set(list(missing.keys()))):
         part_price+=tgt_price
@@ -33,17 +33,20 @@ def backtracking(idx, part_price, missing):
             if key in missing:
                 missing[key]-=element
             if missing[key]<0: del missing[key]
-        backtracking(idx+1, part_price, missing)
+        backtracking(idx+1, part_price, missing.copy())
     return
 
 def checkout(skus):
     global min_price
+    min_price=float("inf")
     buy=Counter(skus)
     if not set(buy.keys()) <= set(prices.keys()): return -1
     backtracking(0, 0, buy)
-    print(min_price)
     return min_price
 
 
 
 assert checkout("AAA")==130
+assert checkout("AAAAAA")==250
+assert checkout("AAAAA")==200
+assert checkout("AA")==100
