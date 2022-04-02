ed316325a61f637365f1b9d853a1b24d689b21b4
sbx_user1051 <sbx_user1051@169.254.136.165>
2022-04-02 11:51:11 +0000

    Sat Apr 02 11:51:11 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,7 +1,22 @@
+from collections import Counter
+
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
+    buy=Counter(skus)
     prices={"A":50, "B":30, "C":20, "D":15}
     offers={"A":[3, 130], "B":[2, 45]}
-    return 15
+    if not set(buy.keys()) <= set(prices.keys()): return -1
+    for key, element in buy.items():
+        price_item=0
+        if key in offers:
+            offer=offers[key]
+            while element>=offer[0]:
+                element-=offer[0]
+                price_item+=offer[1]
+            price_item+=element*prices[key]
+            price+=price_item
+
+    return price
+
 
